d707402ede866e8fd1128e5f1ef33fe99fd4be05
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-06 19:16:51 -0700

    tidied

--- Leaflet/static/js/logic.js
+++ Leaflet/static/js/logic.js
@@ -1,16 +1,3 @@
-/*
-
-Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. 
-
-Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
-
-Hint: The depth of the earth can be found as the third coordinate for each earthquake.
-
-Include popups that provide additional information about the earthquake when its associated marker is clicked.
-
-Create a legend that will provide context for your map data.
-
- */
 
 const URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
@@ -22,105 +9,18 @@ const elMapo = L.map("map", {
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(elMapo);
-let dooo
+
 d3.json(URL).then(data => {
-  dooo = data
     data.features.forEach(feature => {
         const { properties: { mag }, geometry:{coordinates: [lat, lon, depth] }} = feature
+
         L.circleMarker([lat,lon], {
           color: `rgb(${scale(depth,-10,601,255,0)})`,      
           radius: scale(mag, 0,10,10,100)     
       }).addTo(elMapo).bindPopup(lat,lon,mag,depth);
     })
-    console.log(data)
 });
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 } //from stackoverfow
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Creating the map object
-var myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  // Use this link to get the GeoJSON data.
-  var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
-  
-  // The function that will determine the color of a neighborhood based on the borough that it belongs to
-  function chooseColor(borough) {
-    if (borough == "Brooklyn") return "yellow";
-    else if (borough == "Bronx") return "red";
-    else if (borough == "Manhattan") return "orange";
-    else if (borough == "Queens") return "green";
-    else if (borough == "Staten Island") return "purple";
-    else return "black";
-  }
-  
-  // Getting our GeoJSON data
-  d3.json(link).then(function(data) {
-    // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data, {
-      // Styling each feature (in this case, a neighborhood)
-      style: function(feature) {
-        return {
-          color: "white",
-          // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
-          fillColor: chooseColor(feature.properties.borough),
-          fillOpacity: 0.5,
-          weight: 1.5
-        };
-      },
-      // This is called on each feature.
-      onEachFeature: function(feature, layer) {
-        // Set the mouse events to change the map styling.
-        layer.on({
-          // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
-          mouseover: function(event) {
-            layer = event.target;
-            layer.setStyle({
-              fillOpacity: 0.9
-            });
-          },
-          // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 50%.
-          mouseout: function(event) {
-            layer = event.target;
-            layer.setStyle({
-              fillOpacity: 0.5
-            });
-          },
-          // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
-          click: function(event) {
-            myMap.fitBounds(event.target.getBounds());
-          }
-        });
-        // Giving each feature a popup with information that's relevant to it
-        layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
-  
-      }
-    }).addTo(myMap);
-  });
-   */
